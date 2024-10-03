bd2c1554b0780702b8f4cc5bf02191414df74bae
Hanish Rishen <hanishrishen@gmail.com>
2024-10-03 21:14:09 +0530

    Hydration erro fix 2

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -78,7 +78,7 @@ export const PinPerspective = ({
   href?: string;
 }) => {
   return (
-    <motion.div className='pointer-events-none z-[60] flex h-80 w-96 items-center justify-center opacity-0 transition duration-500 group-hover/pin:opacity-100'>
+    <div className='pointer-events-none z-[60] flex h-80 w-96 items-center justify-center opacity-0 transition duration-500 group-hover/pin:opacity-100'>
       <div className='inset-0 -mt-7 h-full w-full flex-none'>
         <div className='absolute inset-x-0 top-0 flex justify-center'>
           <a
@@ -101,7 +101,7 @@ export const PinPerspective = ({
           }}
           className='absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2'
         >
-          <>
+          <div>
             <motion.div
               initial={{
                 opacity: 0,
@@ -162,16 +162,16 @@ export const PinPerspective = ({
               }}
               className='absolute left-1/2 top-1/2 h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]'
             ></motion.div>
-          </>
+          </div>
         </div>
 
-        <>
+        <div>
           <motion.div className='absolute bottom-1/2 right-1/2 h-20 w-px translate-y-[14px] bg-gradient-to-b from-transparent to-cyan-500 blur-[2px] group-hover/pin:h-40' />
           <motion.div className='absolute bottom-1/2 right-1/2 h-20 w-px translate-y-[14px] bg-gradient-to-b from-transparent to-cyan-500 group-hover/pin:h-40' />
           <motion.div className='absolute bottom-1/2 right-1/2 z-40 h-[4px] w-[4px] translate-x-[1.5px] translate-y-[14px] rounded-full bg-cyan-600 blur-[3px]' />
           <motion.div className='absolute bottom-1/2 right-1/2 z-40 h-[2px] w-[2px] translate-x-[0.5px] translate-y-[14px] rounded-full bg-cyan-300' />
-        </>
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
