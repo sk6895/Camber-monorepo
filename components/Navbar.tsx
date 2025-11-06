75dc3323422ac68ef82b84658b0e2dfa666ed8d5
Adhitya <adhitya.senthil22@gmail.com>
2025-11-07 00:06:21 +0530

    Increased SRM logo size in Navbar Furthur as demanded

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,13 +38,13 @@ export function Navbar() {
       <div className={cn('fixed inset-x-6 top-6 z-50')}>
         <div className='rounded-2xl border border-gray-200 bg-white px-4 py-4 shadow-lg dark:border-gray-700 dark:bg-gray-800 md:px-10 md:py-3'>
           <div className='flex items-center justify-between'>
-            <div className='h-18 flex items-center'>
+            <div className='h-20 flex items-center'>
               <Image
                 src='/partners/SRM IST.png'
                 alt='Partner Logo'
-                width={150}
-                height={50}
-                className='h-full w-auto object-contain'
+                width={200}
+                height={200}
+                className='h-full w-auto object-scale-down'
                 priority
               />
             </div>
